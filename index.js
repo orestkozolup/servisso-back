ed0ea2f506536480e9ee534e934251d93297458c
Orest Kozolup <okozol@softserveinc.com>
2023-12-20 23:41:26 +0100

    Add api handlers

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
+const apiPrefix = "/api/v1";
+
+const mockCar = {
+  brand: "Volkswagen",
+  model: "Golf",
+  prod_year: 2008,
+  odometer: 223500,
+  owner_id: "zajebisty_kierowca",
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Jebac pis");
 });
 
-app.get("/haslo", (req, res) => {
-  res.send("hwdp");
+app.get(`${apiPrefix}/cars/:id`, (req, res) => {
+  res.status(200).json({
+    ...mockCar,
+    car_id: req.params.id,
+  });
 });
+
+app.post(`${apiPrefix}/cars`, (req, res) => {
+  res.status(201).json({
+    ...req.body,
+    car_id: "newly_created_car",
+  });
+});
+
+app.put(`${apiPrefix}/cars/:id`, (req, res) => {
+  res.status(200).json({
+    ...req.body,
+    car_id: req.params.id,
+  });
+});
+
+app.delete(`${apiPrefix}/cars/:id`, (req, res) => {
+  res.status(204).end();
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
