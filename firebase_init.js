a5e37beed9201b56a0f3f60ece19efb44415da7c
Orest Kozolup <kozolup.yakiv@gmail.com>
2023-12-23 11:29:02 +0100

    Initial connect to DB

---
+++ firebase_init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+const { initializeApp } =require("firebase/app");
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB_3c2uQuTGw14avDTt4vP4-mfCPmuII4",
+  authDomain: "servisso.firebaseapp.com",
+  projectId: "servisso",
+  storageBucket: "servisso.appspot.com",
+  messagingSenderId: "416973749351",
+  appId: "1:416973749351:web:8cba2aabf21ef67dc1a8c2"
+};
+
+// Initialize Firebase
+module.exports = initializeApp(firebaseConfig);
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const { CAR_FIELDS } = require("./cars/const");
 const { validate } = require("./cars/validators");
 
+const firebaseConfig = require('./firebase_init');
+const { getFirestore, collection, getDocs, getDoc, doc } = require('firebase/firestore/lite');
+
 const { BRAND, MODEL, PRODUCTION_YEAR, OWNER_ID, ODOMETER } = CAR_FIELDS;
 
 const app = express();
@@ -21,14 +24,32 @@ const mockCar = {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+const db = getFirestore(firebaseConfig);
+
+async function getCar(id) {
+  const snap = await getDoc(doc(db, 'cars', id))
+  return snap.exists() ? snap.data() : {}
+}
+
+// async function getCars() {
+//   const carsCol = collection(db, 'cars');
+//   const carSnapshot = await getDocs(carsCol);
+//   const carList = carSnapshot.docs.map(doc => doc.data());
+//   return carList;
+// }
+
 app.get("/", (req, res) => {
   res.send("This is Servisso base API endpoint");
 });
 
-app.get(`${apiPrefix}/cars/:id`, (req, res) => {
+app.get(`${apiPrefix}/cars/:id`, async (req, res) => {
+  const car = await getCar(req.params.id);
+
   res.status(200).json({
-    ...mockCar,
-    car_id: req.params.id,
+    ...car,
+    id: req.params.id,
   });
 });
 
