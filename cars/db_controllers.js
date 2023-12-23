1921dbd4627039893f2113067856a29993986117
Orest Kozolup <kozolup.yakiv@gmail.com>
2023-12-23 13:08:10 +0100

    Format files

--- cars/db_controllers.js
+++ cars/db_controllers.js
@@ -1,33 +1,47 @@
-const { collection, getDocs, getDoc, doc, setDoc, addDoc, deleteDoc } = require('firebase/firestore/lite');
+const {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+} = require("firebase/firestore/lite");
 const db = require("../firebase_init");
 
 async function getCar(id) {
-    const snap = await getDoc(doc(db, 'cars', id))
-    return snap.exists() ? snap.data() : {}
-  }
-  
-  async function createCar(data) {
-    const docRef = await addDoc(collection(db, "cars"), data);
-    return {...(await getDoc(docRef)).data(), id: docRef.id};
-  }
-  
-  async function updateCar(id, data) {
-    const docRef = doc(db, "cars", id);
-    await setDoc(docRef, data);
-    return {...(await getDoc(docRef)).data(), id: docRef.id};
-  }
-  
-  async function deleteCar(id){
-    await deleteDoc(doc(db, "cars", id));
-  }
-  
-  async function getCars() {
-    const carsCol = collection(db, 'cars');
-    const carSnapshot = await getDocs(carsCol);
-    const carList = carSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-    return carList;
-  }
+  const snap = await getDoc(doc(db, "cars", id));
+  return snap.exists() ? snap.data() : {};
+}
+
+async function createCar(data) {
+  const docRef = await addDoc(collection(db, "cars"), data);
+  return { ...(await getDoc(docRef)).data(), id: docRef.id };
+}
+
+async function updateCar(id, data) {
+  const docRef = doc(db, "cars", id);
+  await setDoc(docRef, data);
+  return { ...(await getDoc(docRef)).data(), id: docRef.id };
+}
+
+async function deleteCar(id) {
+  await deleteDoc(doc(db, "cars", id));
+}
+
+async function getCars() {
+  const carsCol = collection(db, "cars");
+  const carSnapshot = await getDocs(carsCol);
+  const carList = carSnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return carList;
+}
 
 module.exports = {
-    getCar, createCar, deleteCar, updateCar
-}
+  getCar,
+  createCar,
+  deleteCar,
+  updateCar,
+};
--- firebase_init.js
+++ firebase_init.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-const { initializeApp } =require("firebase/app");
+const { initializeApp } = require("firebase/app");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,10 +10,10 @@ const firebaseConfig = {
   projectId: "servisso",
   storageBucket: "servisso.appspot.com",
   messagingSenderId: "416973749351",
-  appId: "1:416973749351:web:8cba2aabf21ef67dc1a8c2"
+  appId: "1:416973749351:web:8cba2aabf21ef67dc1a8c2",
 };
 
-const { getFirestore } = require('firebase/firestore/lite');
+const { getFirestore } = require("firebase/firestore/lite");
 
 // Initialize Firebase
 module.exports = getFirestore(initializeApp(firebaseConfig));
